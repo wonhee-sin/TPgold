f6bc378904facbc0c57b4bcea74dc2527a9b219d
wonhee-sin <ww1700@naver.com>
2022-06-26 19:10:44 +0900

    homework

--- 신원희/js/common.js
+++ 신원희/js/common.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
 
-     $("#email_wrap").click(function(){
-          $(".mainEmail").focus();
-          $(".email_text").animate({top : 10, "font-size" : "10px"} ,120);
+     $("#email_wrap1").click(function(){
+          $('.mainEmail1').focus();
+          $('.email_text1').animate({top : 10, "font-size" : "10px"} ,120);
           return false;
      });
 
      $(window).click(function(){
-          if($(".mainEmail").val() == "") 
-          $(".email_text").animate({top : 25, "font-size" : "16px"} ,120);
+          if($(".mainEmail1").val() == "") 
+          $(".email_text1").animate({top : 25, "font-size" : "16px"} ,120);
      })
 
+     $("#email_wrap2").click(function(){
+          $('.mainEmail2').focus();
+          $('.email_text2').animate({top : 10, "font-size" : "10px"} ,120);
+          return false;
+     });
+
+     $(window).click(function(){
+          if($(".mainEmail2").val() == "") 
+          $(".email_text2").animate({top : 25, "font-size" : "16px"} ,120);
+     })
+
+
+     $('.accordion .title').click(function(){
+          $(this).siblings('.accordion .content').stop().slideUp();
+          $(this).next().stop().slideToggle('fast');
+          $(this).siblings('.accordion .title').removeClass('active');
+          $(this).toggleClass('active')
+     });
 })
